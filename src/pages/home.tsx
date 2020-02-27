dacbc2b8282683b8d9489bee33f61149327a55a7
David Sanders <dsanders_m633@outlook.com>
2020-02-27 14:52:20 -0500

    test commit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,7 +10,7 @@ function Home() {
       <PageContainer title={'Home'}>
         <HomeSlider />
         <PageContent>
-          <h1 className='pt-4'>Home Page foo</h1>
+          <h1 className='pt-4'>Home Page</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et tincidunt tortor, eu finibus felis. Praesent lorem leo, tempus vitae libero volutpat, facilisis consectetur lorem. Maecenas a quam lacus. In eros nulla, finibus id dignissim a, bibendum ac lorem. Donec tristique faucibus ligula id malesuada. Fusce dignissim, ex non viverra maximus, metus est scelerisque ex, at molestie tortor dolor ut diam. Aliquam sodales dolor sed nunc maximus egestas.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et tincidunt tortor, eu finibus felis. Praesent lorem leo, tempus vitae libero volutpat, facilisis consectetur lorem. Maecenas a quam lacus. In eros nulla, finibus id dignissim a, bibendum ac lorem. Donec tristique faucibus ligula id malesuada. Fusce dignissim, ex non viverra maximus, metus est scelerisque ex, at molestie tortor dolor ut diam. Aliquam sodales dolor sed nunc maximus egestas.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et tincidunt tortor, eu finibus felis. Praesent lorem leo, tempus vitae libero volutpat, facilisis consectetur lorem. Maecenas a quam lacus. In eros nulla, finibus id dignissim a, bibendum ac lorem. Donec tristique faucibus ligula id malesuada. Fusce dignissim, ex non viverra maximus, metus est scelerisque ex, at molestie tortor dolor ut diam. Aliquam sodales dolor sed nunc maximus egestas.</p>
