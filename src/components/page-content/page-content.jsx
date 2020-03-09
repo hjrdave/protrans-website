cdd6f1a2f158efd934d35e92346667b8029cbcdd
David Sanders <dsanders_m633@outlook.com>
2020-03-09 16:15:55 -0400

    added disqus comments to blog posts.

--- src/components/page-content/page-content.jsx
+++ src/components/page-content/page-content.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 
-function PageContent({ children }) {
+function PageContent({ children, type }) {
   return (
     <>
       <div className="container-fluid pt-4">
         <div className="row d-flex justify-content-center">
-          <div className="col-8">
+          <div className={`${(type === 'post') ? 'col-6' : 'col-8'}`}>
             <>{children}</>
           </div>
         </div>
--- src/templates/post-template.jsx
+++ src/templates/post-template.jsx
@@ -4,17 +4,20 @@ import Img from "gatsby-image";
 import PageContainer from "../components/page-container";
 import PageContent from "../components/page-content";
 import ShareButton from '../components/share-button';
+import { DiscussionEmbed } from 'disqus-react'
 
 function PostTemplate({ data, location }) {
   const { markdownRemark, site } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
   const { siteMetadata } = site;
   const { title, date, featuredImage, category, path } = frontmatter;
+  const disqusShortname = "protrans";
+  const disqusConfig = { identifier: path, title: title }
 
   return (
     <>
       <PageContainer title={title} location={location}>
-        <PageContent>
+        <PageContent type='post'>
           <Img fluid={featuredImage.childImageSharp.fluid} />
           <p className='pt-4'>Category: {category}</p>
           <p>{date}</p>
@@ -29,6 +32,7 @@ function PostTemplate({ data, location }) {
             className="blog-post-content pt-4"
             dangerouslySetInnerHTML={{ __html: html }}
           />
+          <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
         </PageContent>
       </PageContainer>
     </>
@@ -38,28 +42,28 @@ function PostTemplate({ data, location }) {
 //graphQL query
 export const pageQuery = graphql`
   query($path: String!) {
-    site {
-      siteMetadata {
-        domain: siteUrl
-      }
-    }
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
+              site {
+              siteMetadata {
+              domain: siteUrl
+          }
+        }
+    markdownRemark(frontmatter: {path: {eq: $path } }) {
+              html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        path
-        title
-        category
+              date(formatString: "MMMM DD, YYYY")
+            path
+            title
+            category
         featuredImage {
-          childImageSharp {
-            fluid(maxWidth: 600) {
+              childImageSharp {
+              fluid(maxWidth: 600) {
               ...GatsbyImageSharpFluid
+            }
             }
           }
         }
       }
     }
-  }
-`
+  `
 
-export default PostTemplate
+export default PostTemplate;
