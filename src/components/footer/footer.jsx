108ca70091cf88344c5f0f5d9067dd302312ef52
David Sanders <dsanders_m633@outlook.com>
2020-03-10 13:58:17 -0400

    added sticky top nav

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,45 +6,52 @@ function Footer() {
   return (
     <>
       <footer className="mt-4">
-        <div className='p-4 d-flex justify-content-between footer-content'>
-          <div className='d-flex'>
-            <div className='footer-apps-logins mr-4'>
-              <p className='text-uppercase font-weight-bold'>Apps and Logins</p>
-              <ul>
-                <li><Link to='/'>Optimiz</Link></li>
-                <li><Link to='/'>Business Intelligence</Link></li>
-                <li><Link to='/'>Applications</Link></li>
-                <li><Link to='/'>Claims</Link></li>
-                <li><Link to='/'>ADP</Link></li>
-                <li><Link to='/'>Track Shipment</Link></li>
-              </ul>
-            </div>
-            <div className='footer-contact-us ml-4'>
-              <p className='text-uppercase font-weight-bold'>Contact Us</p>
-              <ul>
-                <li><Link to='/'>Sales Inquires</Link></li>
-                <li><Link to='/'>Customer Service</Link></li>
-                <li><Link to='/'>Create a Shipment</Link></li>
-                <li><Link to='/'>Locations</Link></li>
-              </ul>
+        <div className='d-flex justify-content-center'>
+          <div className='col-8'>
+            <div className='p-4 d-flex justify-content-between footer-content'>
+              <div className='d-flex'>
+                <div className='footer-apps-logins mr-4'>
+                  <p className='text-uppercase font-weight-bold'>Apps and Logins</p>
+                  <ul>
+                    <li><Link to='/'>Optimiz</Link></li>
+                    <li><Link to='/'>Business Intelligence</Link></li>
+                    <li><Link to='/'>Applications</Link></li>
+                    <li><Link to='/'>Claims</Link></li>
+                    <li><Link to='/'>ADP</Link></li>
+                    <li><Link to='/'>Track Shipment</Link></li>
+                  </ul>
+                </div>
+                <div className='footer-contact-us ml-4'>
+                  <p className='text-uppercase font-weight-bold'>Contact Us</p>
+                  <ul>
+                    <li><Link to='/'>Sales Inquires</Link></li>
+                    <li><Link to='/'>Customer Service</Link></li>
+                    <li><Link to='/'>Create a Shipment</Link></li>
+                    <li><Link to='/'>Locations</Link></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className='footer-social-media'>
+                <p className='text-uppercase font-weight-bold text-center'>Social Media</p>
+                <div className='footer-social-media-icons d-flex'>
+                  <a href='/' target='_blank'><i class="fab fa-facebook px-1"></i></a>
+                  <a href='/' target='_blank'><i class="fab fa-twitter px-1"></i></a>
+                  <a href='/' target='_blank'><i class="fab fa-linkedin px-1"></i></a>
+                  <a href='/' target='_blank'><i class="fab fa-youtube px-1"></i></a>
+                  <a href='/' target='_blank'><i class="fab fa-instagram px-1"></i></a>
+                </div>
+              </div>
+
             </div>
           </div>
-
-          <div className='footer-social-media'>
-            <p className='text-uppercase font-weight-bold text-center'>Social Media</p>
-            <div className='footer-social-media-icons d-flex'>
-              <a href='/' target='_blank'><i class="fab fa-facebook px-1"></i></a>
-              <a href='/' target='_blank'><i class="fab fa-twitter px-1"></i></a>
-              <a href='/' target='_blank'><i class="fab fa-linkedin px-1"></i></a>
-              <a href='/' target='_blank'><i class="fab fa-youtube px-1"></i></a>
-              <a href='/' target='_blank'><i class="fab fa-instagram px-1"></i></a>
-            </div>
-          </div>
-
         </div>
         <div className='footer-copyright p-2 px-3 d-flex justify-content-between'>
+          <p className='mb-0 text-uppercase' >
+            <small>Site Map | Privicy Policy | Terms of Use | Careers</small>
+          </p>
           <p className='mb-0'><small>&copy; 1993 - {new Date().getFullYear()}, ProTrans International Inc. All Rights Reserved.</small></p>
-          <p className='mb-0 text-uppercase'>
+          <p className='mb-0 text-uppercase' style={{ opacity: '0' }}>
             <small><Link to='/'>Site Map</Link> | <Link to='/'>Privicy Policy</Link> | <Link to='/'>Terms of Use</Link> | <Link to='/'>Careers</Link></small>
           </p>
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,9 @@
-import { Link } from "gatsby"
-import React, { useEffect } from "react"
-import "./_header.scss"
-import Image from "../../images/register"
-import MainNav from "../../widgets/main-nav"
+import { Link } from "gatsby";
+import React, { useEffect } from "react";
+import "./_header.scss";
+import Image from "../../images/register";
+import MainNav from "../../widgets/main-nav";
+import Sticky from 'react-stickynode';
 
 function Header({ siteTitle }) {
   useEffect(() => {
@@ -11,7 +12,7 @@ function Header({ siteTitle }) {
   return (
     <>
       <div class='header-top d-flex justify-content-end p-1 pr-4'>
-        <p className='mb-0'>
+        <p className='mb-0 py-1'>
           <small>
             <a href='/' target='_blank' className='px-1'>Apps</a>  |
             <a href='/' target='_blank' className='px-1'>Business Intel</a>  |
@@ -20,16 +21,18 @@ function Header({ siteTitle }) {
           </small>
         </p>
       </div>
-      <header className="px-4 d-flex justify-content-between align-content-center">
-        <div className="header-logo pl-4 pt-1">
-          <Link to="/">
-            <Image name={"logoProtrans"} />
-          </Link>
-        </div>
-        <div className='header-nav pr-4'>
-          <MainNav />
-        </div>
-      </header>
+      <Sticky enabled={true} enableTransforms={true} activeClass={'header-sticky'}>
+        <header className="px-4 d-flex justify-content-between align-content-center">
+          <div className="header-logo pl-4 pt-1">
+            <Link to="/">
+              <Image name={"logoProtrans"} />
+            </Link>
+          </div>
+          <div className='header-nav pr-4'>
+            <MainNav />
+          </div>
+        </header>
+      </Sticky>
     </>
   )
 }
